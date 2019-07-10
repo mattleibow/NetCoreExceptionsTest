bd908a9578da661355d18648776f104089b60170
Matthew Leibowitz <mattleibow@live.com>
2019-07-10 17:55:23 +0200

    Fixes

--- build.ps1
+++ build.ps1
@@ -6,7 +6,9 @@ if ($IsLinux) {
 } elseif ($IsMacOS) {
     xcodebuild -project native/nativelibrary/nativelibrary.xcodeproj
 } else {
-    msbuild native/libnativelibrary.vcxproj /p:Platform=x64 /v:m
+    $vswhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+    $msbuild = & $vswhere -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe | select-object -first 1 
+    & $msbuild native/libnativelibrary.vcxproj /p:Platform=x64 /v:m
 }
 
 Write-Host ""
