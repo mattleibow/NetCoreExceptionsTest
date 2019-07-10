2114fcb7ba9fcc237f2af701bdbd73ae01b1054f
Matthew Leibowitz <mattleibow@live.com>
2019-07-10 17:58:00 +0200

    Spaces?

--- build.ps1
+++ build.ps1
@@ -7,8 +7,8 @@ if ($IsLinux) {
     xcodebuild -project native/nativelibrary/nativelibrary.xcodeproj
 } else {
     $vswhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
-    $msbuild = & $vswhere -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe | select-object -first 1 
-    & $msbuild native/libnativelibrary.vcxproj /p:Platform=x64 /v:m
+    $msbuild = & $vswhere -latest -requires 'Microsoft.Component.MSBuild' -find 'MSBuild\**\Bin\MSBuild.exe' | select-object -first 1 
+    & "$msbuild" native/libnativelibrary.vcxproj /p:Platform=x64 /v:m
 }
 
 Write-Host ""
