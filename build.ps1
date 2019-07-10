f6ed29c8168aaaa291600bf5b0bc2f17ca75ad04
Matthew Leibowitz <mattleibow@live.com>
2019-07-10 18:01:07 +0200

    Windows...

--- build.ps1
+++ build.ps1
@@ -7,7 +7,12 @@ if ($IsLinux) {
     xcodebuild -project native/nativelibrary/nativelibrary.xcodeproj
 } else {
     $vswhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
-    $msbuild = & $vswhere -latest -requires 'Microsoft.Component.MSBuild' -find 'MSBuild\**\Bin\MSBuild.exe' | select-object -first 1 
+
+    & $vswhere -latest -requires 'Microsoft.Component.MSBuild' -find 'MSBuild\**\Bin\MSBuild.exe' | select-object -first 1
+    & $vswhere -latest -requires 'Microsoft.Component.MSBuild' -find 'MSBuild\**\Bin\MSBuild.exe'
+    & $vswhere -latest -requires 'Microsoft.Component.MSBuild'
+
+    $msbuild = & $vswhere -latest -requires 'Microsoft.Component.MSBuild' -find 'MSBuild\**\Bin\MSBuild.exe' | select-object -first 1
     & "$msbuild" native/libnativelibrary.vcxproj /p:Platform=x64 /v:m
 }
 
